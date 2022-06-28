061bda0936eeab10eaa65f2d684f88a5385180e8
Francisco Di Paolo <franciscogermandipaolo@gmail.com>
2022-06-28 16:16:59 -0300

    SlimeUI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -27,7 +27,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("r"))
+        {
+            PlayerPrefs.DeleteAll();
+        }
     }
 
     public void AddDiamond(GameObject diamond)
