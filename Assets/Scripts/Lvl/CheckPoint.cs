ca8bb65b8c16e4770d52b279578c55f2aae1d6a7
RoberP1 <roberpapeschi@gmail.com>
2022-07-12 16:46:37 -0300

    checkpoint anim

--- Assets/Scripts/Lvl/CheckPoint.cs
+++ Assets/Scripts/Lvl/CheckPoint.cs
@@ -6,7 +6,15 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class CheckPoint : MonoBehaviour
 {
+    private Animator animator;
+    private int id;
     public static event Action<Vector3> OnCheckPoint;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        id = Animator.StringToHash("CheckPoint");
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         IPlayer player = collision.GetComponent<IPlayer>();
@@ -14,6 +22,7 @@ public class CheckPoint : MonoBehaviour
         {
             OnCheckPoint?.Invoke(transform.position);
             GetComponent<Collider2D>().enabled = false;
+            animator.SetTrigger(id);
         }
     }
 }
