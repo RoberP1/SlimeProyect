75756aae44275d75eb6bdb432046d174e03842bf
RoberP1 <roberpapeschi@gmail.com>
2022-07-26 19:11:00 -0300

    boss

--- Assets/Scripts/Enemy/Boss/Boss.cs
+++ Assets/Scripts/Enemy/Boss/Boss.cs
@@ -4,15 +4,80 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Health")]
+    [SerializeField] private int health;
+    [SerializeField] private int maxHealth;
+    public bool CanTakeDamage;
+
+
+    
+    [Header("Attack")]
+    [SerializeField] GameObject ProyectilePreFab;
+    [SerializeField] float ProyectileVelocity;
+    [SerializeField] int fireNumber;
+    //animations
+    private Animator animator;
+    private int HitID;
+    private int AttackID;
+    private int DeadID;
+
+    private Transform player;
+
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player").transform;
+        animator = GetComponent<Animator>();
+        health = maxHealth;
+        SetAnimationId();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (CanTakeDamage)
+        {
+            TakeDamage();
+        }
+        else
+        {
+            IPlayer Player = collision.GetComponent<IPlayer>();
+            Player?.InstaKillDamage();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Attack()
     {
-        
+        if (fireNumber > 0)
+        {
+            GameObject proyectile = Instantiate(ProyectilePreFab, transform.position, Quaternion.identity);
+            proyectile.GetComponent<Rigidbody2D>().velocity = (player.position - transform.position) * ProyectileVelocity;
+            animator.SetBool(AttackID, true);
+            fireNumber--;
+        }
+        else
+        {
+            fireNumber = maxHealth - health;
+            animator.SetBool(AttackID, false);
+        }
     }
+
+    public void TakeDamage()
+    {
+        animator.SetBool(HitID, true);
+        health--;
+        if (health <= 0)
+        {
+            animator.SetBool(DeadID, true);
+        }
+    }
+    public void FinishDamageAnimation()
+    {
+        animator.SetBool(HitID, false);
+    }
+
+    private void SetAnimationId()
+    {
+        HitID = Animator.StringToHash("Hit");
+        AttackID = Animator.StringToHash("Attack");
+        DeadID = Animator.StringToHash("Dead");
+    }
+
 }
